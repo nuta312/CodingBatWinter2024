9a69276ed32fc4f7b201f51d2c7a50c61a068756
Nursultan <nuta@gmail.com>
2024-05-14 11:04:22 +0600

    change:Hello Mir

--- src/main/java/org/example/nuta/NutaClass.java
+++ src/main/java/org/example/nuta/NutaClass.java
@@ -2,6 +2,6 @@ package org.example.nuta;
 
 public class NutaClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Mir!!!");
     }
 }
